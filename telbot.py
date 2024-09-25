66ae155b2a515ed9cec7e595d77b2e1ca7e963be
sherlemp28 <110886373+sherlemp28@users.noreply.github.com>
2024-09-25 20:28:29 +0530

    Add files via upload

---
+++ telbot.py
@@ -0,0 +1,16 @@
+import telebot
+#lanprin
+bot = telebot.TeleBot('7437685448:AAE8wsoD5AYC1ROL_4MukCfxY5jJTmiFHFY')
+
+# Handler for /start command
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Hi", parse_mode='html')
+
+# Handler for text messages
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text, parse_mode='html')
+
+# Start polling for messages
+bot.infinity_polling(timeout=10, long_polling_timeout=5)
